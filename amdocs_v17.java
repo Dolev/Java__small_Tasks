ca82c9730d9d60659468a8a3159f3f440f387014
Dolev .H <33119992+Dolev@users.noreply.github.com>
2022-06-21 14:51:29 +0300

    Update amdocs_v17.java

--- amdocs_v17.java
+++ amdocs_v17.java
@@ -1,1 +1,25 @@
 
+class Solution {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        int n =A.length;
+        double sum=0;
+        double curr=0;
+        for(int i=0;i<n;i++){
+            curr=A[i]*Math.pow(10, i);
+            sum+=curr;
+        }
+        int int_sum=(int)sum;
+        int Mul_ST=17*int_sum;
+        int ans=0;
+        int digit=0;
+        while(Mul_ST > 0)  
+        {     
+        digit = Mul_ST % 10;  
+        ans = ans + digit;  //adds last digit to the variable ans             
+        Mul_ST = Mul_ST / 10; //removes the last digit 
+        } 
+
+        return ans;
+    }
+}
